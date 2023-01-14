f6b410fb0a61df4178cd36af088c8668cce5a4b8
snrvivo <63261648+snrvivo@users.noreply.github.com>
2023-01-14 14:14:19 +0000

    Update app.py

--- app.py
+++ app.py
@@ -6,7 +6,13 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return "Hello Senor Vivo! " + str(datetime.datetime.now())
+    f = open("/data/test.txt","r")
+    lines = f.readlines()
+
+    var inTest = ""
+    for line in lines:
+        inTest = inTest + line
+    return "Hello Senor Vivo! " + str(datetime.datetime.now()) + " " + inTest
 
 if __name__ == '__main__':
     port = os.environ.get('FLASK_PORT') or 8080
